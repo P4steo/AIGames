feaa4dfb776dd3695059fbd4ef035a326e78bb18
Agata <s24305@pjwstk.edu.pl>
2022-11-19 23:08:17 +0100

    Print from npz file a dataframe from pandas

--- read_data/read_npz.py
+++ read_data/read_npz.py
@@ -1,7 +1,19 @@
 import numpy
+import pandas as pd
+import scipy
+from scipy import sparse
 
 data = numpy.load('../npz/plik1.npz')
 lst = data.files
 for item in lst:
     print(item)
     print(data[item])
+
+sparsed_data = scipy.sparse.load_npz('../npz/plik1.npz')
+df = pd.DataFrame.sparse.from_spmatrix(sparsed_data)
+print(df)
+
+# df = pd.DataFrame.from_dict({item: data[item] for item in data.files}, orient='index')
+# dd = pd.DataFrame.from_dict({item: [data[item]] for item in data.files}, orient='index')
+# print(dd.head())
+
