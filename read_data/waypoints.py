28702ac405ea298b004c124c8b819d7cc5ab480c
Pablo <s24810@pjwstk.edu.pl>
2022-11-19 17:08:08 +0100

    słownik id:waypoints

--- read_data/waypoints.py
+++ read_data/waypoints.py
@@ -7,12 +7,19 @@ with open('../datasets/route_definitions.csv', 'r') as file:
         lenline = len(line)
         data.append(line)
     waypoints = []
-    waypoints_og = []
+    waypoints_id = []
+    waypoints_og = []  # temporary array
+    id_waypoints = {}
     licznik = 1
     for i in data:
         licznik += 1
         if len(i) > 0:
             waypoints_og = i.split("\"")
             waypoints.append(waypoints_og[1])
-    for element in waypoints:
-        print(element)
+            waypoints_id.append((waypoints_og[0].split(","))[0])
+
+    for k in range(0, len(waypoints)):
+        id_waypoints[waypoints_id[k]] = waypoints[k]
+
+    for i in id_waypoints:
+        print("{i}:{value}".format(i=i, value=id_waypoints[i]))
