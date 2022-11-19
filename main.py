e673e0b7e425b5900519ff9f783ab7ea54c51d2e
Agata <s24305@pjwstk.edu.pl>
2022-11-19 12:50:42 +0100

    Add different files to reader and print in main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import read_data.readdata as reader
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(reader.route_definitions.head())
+print(reader.test_observations.head())
+print(reader.test_observations_1000.head())
+print(reader.train_availability.head())
+print(reader.train_observations.head())
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm is cool false')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- read_data/readdata.py
+++ read_data/readdata.py
@@ -1,5 +1,7 @@
 import pandas as p
 
 route_definitions = p.read_csv('datasets/route_definitions.csv')
-
-print(route_definitions.to_string())
+test_observations = p.read_csv('datasets/test_observations.csv')
+test_observations_1000 = p.read_csv('datasets/test_observations_1000.csv')
+train_availability = p.read_csv('datasets/train_availability.csv')
+train_observations = p.read_csv('datasets/train_observations.csv')
