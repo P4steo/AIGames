10c995ee8c68384fb52361159f00f25c54ed71f2
Pablo <s24810@pjwstk.edu.pl>
2022-11-19 21:55:32 +0100

    initial commit

--- read_data/readdata.py
+++ read_data/readdata.py
@@ -6,5 +6,3 @@ test_observations = p.read_csv('datasets/test_observations.csv')
 test_observations_1000 = p.read_csv('datasets/test_observations_1000.csv')
 train_availability = p.read_csv('datasets/train_availability.csv')
 train_observations = p.read_csv('datasets/train_observations.csv')
-
-
