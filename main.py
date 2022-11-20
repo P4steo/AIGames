d68e389008e981c6e6e2a32b5a86157ed3fb85e6
Agata <s24305@pjwstk.edu.pl>
2022-11-20 06:04:31 +0100

    Making ACTUAL prediction and saving it to file as we are SUPPOSED to do

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
+import predictions as predictions
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 
 
 def convert_date_to_int(datetime):
-    return int(datetime.strftime("%Y%m%d%H%M"))
+    return int(datetime.strftime("%H"))
 
 
 # read data by pandas
@@ -36,3 +36,34 @@ clf = DecisionTreeClassifier().fit(observations[:train_ids], availability[:train
 y_pred = clf.predict(observations[train_ids:])
 print(accuracy_score(availability[train_ids:].status, y_pred))
 print(clf.score(observations[train_ids:], availability[train_ids:].status))
+
+# save predictions
+
+# prepare testing data
+test_data = pd.read_csv("datasets/test_observations_1000.csv")
+test_data['timestamp'] = pd.to_datetime(test_data['timestamp'])
+test_data['timestamp'] = test_data['timestamp'].apply(lambda d: convert_date_to_int(d))
+label_encoder = preprocessing.LabelEncoder()
+test_data['route_id'] = label_encoder.fit_transform(test_data['route_id'])
+
+# make predictions
+prediction = clf.predict(test_data)
+
+predicted_observation_id = []
+predicted_status = []
+
+# append data from predictions
+for result in range(0, len(prediction)):
+    status = ""
+    if prediction[result] == 1:
+        status = "OPEN"
+    else:
+        status = "CLSD"
+    predicted_observation_id.append(test_data['observation_id'][result])
+    predicted_status.append(status)
+
+result_data_dict = {'observation_id': predicted_observation_id, 'status': predicted_status}
+
+# save data to file
+result_df = pd.DataFrame(result_data_dict)
+result_df.to_csv('test_availability.csv', index=False)
