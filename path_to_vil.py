a9ee8d39b5e282b9c113f7bfa8520d4abf074862
Pablo <s24810@pjwstk.edu.pl>
2022-11-20 02:23:32 +0100

    initial commit

--- path_to_vil.py
+++ path_to_vil.py
@@ -1,14 +1,14 @@
-def choose_vil_path(y,d,m):
+def choose_vil_path(y, d, m):
     path = ""
-    if [2020, 1, 1] <= [y,d,m] <= [2020, 3, 31]:
+    if [2020, 1, 1] <= [y, d, m] <= [2020, 3, 31]:
         path += "VIL_merc_2020_01-03"
-    elif [2020, 4, 1] <= [y,d,m] <= [2020, 5, 31]:
+    elif [2020, 4, 1] <= [y, d, m] <= [2020, 5, 31]:
         path += "VIL_merc_2020_04-06"
-    elif [2020, 7, 1] <= [y,d,m] <= [2020, 12, 31]:
+    elif [2020, 7, 1] <= [y, d, m] <= [2020, 12, 31]:
         path += "VIL_merc_2020_07-12"
-    elif [2021, 1, 1] <= [y,d,m] <= [2021, 5, 31]:
+    elif [2021, 1, 1] <= [y, d, m] <= [2021, 5, 31]:
         path += "VIL_merc_2021_01-06"
-    elif [2021, 7, 1] <= [y,d,m] <= [2021, 12, 31]:
+    elif [2021, 7, 1] <= [y, d, m] <= [2021, 12, 31]:
         path += "VIL_merc_2021_07-12"
     else:
         path = ""
@@ -39,8 +39,7 @@ with open('datasets/train_observations.csv', 'r') as file:
     for element in data_timestamp:
         data_temp.append(element.split(" ")[0])
     for k in data_temp:
-        y=int(k.split("-")[0])
-        d=int(k.split("-")[1])
-        m=int(k.split("-")[2])
-        print(choose_vil_path(y,d,m))
-
+        y = int(k.split("-")[0])
+        d = int(k.split("-")[1])
+        m = int(k.split("-")[2])
+        print(choose_vil_path(y, d, m))
